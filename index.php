83be412ae415725d6b9900fffbfd7d88b45d56a6
MohdHassangit <33454779+MohdHassangit@users.noreply.github.com>
2018-01-23 15:18:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,69 @@
 <?php
 session_start();
-
+require 'gmail.php';
 //Create a session of username and logging in the user to the chat room
-if(isset($_POST['username'])){
-	$_SESSION['username']=$_POST['username'];
+if(isset($_POST['password'])){
 
 //Unset session and logging out user from the chat room
 
-if ($_POST['username'] == "321" or $_POST['username']=="567") {
-  $_SESSION['username']=$_POST['username']; ;
-} else {
- unset($_SESSION['username']);
+if ($_POST['password'] == "567" && $_POST['username'] != null) {
+ $_SESSION['username']=trim(htmlentities(trim($_POST['username']), ENT_NOQUOTES, 'utf-8'));
+
+  $_SESSION['password']=htmlspecialchars($_POST['password']);
+$_SESSION['function']="onload='myFunctions();'";
+ $_SESSION['functionget']="<script src='chat.js'></script>";
+ $_SESSION['clear']=null;
+$mail->addAddress('mdhassandbg7352@gmail.com', $_SESSION['username']);     // Add a recipient
+ $mail->addAddress('Inzemamhaq05@gmail.com', $_SESSION['username']); 
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Roboticeye';
+    $mail->Body    = $_SESSION['username']." is online
+<br><a href='http://roboticeye.tk'>click here</a>
+
+";
+
+   $mail->send();  
+
+} elseif($_POST['password']=="654321") { 
+ $_SESSION['username']=$_POST['username']; 
+  $_SESSION['password']=$_POST['password'];
+$_SESSION['function']="onload='myFunctions();'";
+ $_SESSION['functionget']="<script src='chat.js'></script>";
+ $_SESSION['clear']="<div class='actions'> <i name='clear'  id='clear'>
+<img  src='clear.png'   height=25px width=25px>
+                </i> 
+              </div>";
+
+
+}
+
+elseif($_POST['password']=="7352") { 
+ $_SESSION['username']=$_POST['username']; 
+  $_SESSION['password']=$_POST['password'];
+$_SESSION['function']="onload='myFunctions();'";
+ $_SESSION['functionget']="<script src='chat.js'></script>";
+ $_SESSION['clear']="<div class='actions'> <i name='clear'  id='clear'>
+<img  src='clear.png'   height=25px width=25px>
+                </i> 
+              </div>";
+
+}
+
+
+else
+{
+ unset($_SESSION['password']);
  echo "<div class='danger'>
-  <p><strong>Error</strong> incorrect passcode</p>
+  <p><strong>Error!</strong> incorrect passcode </p>
 </div>";
 
 }
 }
 
 if(isset($_GET['logout'])){
-	unset($_SESSION['username']);
+	unset($_SESSION['password']);
 	header('Location:index.php');
 }
 
@@ -27,197 +71,44 @@ if(isset($_GET['logout'])){
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
-<meta name="theme-color" content="#1a598c" />
+  
+<meta name="theme-color" content="#000000" />
 <meta name="mobile-web-app-capable" content="yes">
-	<title>My Chat Room</title>
+<meta property="og:url" content="http://roboticeye
+tk/" />
+ <meta property="og:type" content="Robotic eye" /> 
+<meta property="og:title" content="Roboticeye" /> 
+<meta property="og:description" content="Roboticeye" /> 
+<meta property="og:image" content="http://roboticeye.tk/robotic.png" />
+	<title>Robotic Eye</title>
+    <link rel="shortcut icon" href="hello.png">
+
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.1.2/css/material-design-iconic-font.min.css" />
 <link rel="stylesheet" href="https://rawgit.com/marvelapp/devices.css/master/assets/devices.min.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700,300" />
-<link rel="stylesheet" href="newcss.php" />
+<link rel="stylesheet" href="newcss2.php" />
  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="Image-Upload-Preview-Plugin-With-jQuery-Bootstrap-img-upload/dist/css/bootstrap-imageupload.css" rel="stylesheet">
 	<script type="text/javascript" src="jquery-1.10.2.min.js" ></script>
-     <script src="input.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
        
 <style>
 body{
-    background-color: #1a598c91;
+    background-color: #000000;
 }
 
-.danger {
-    background-color: #f58484;
-    border-left: 6px solid #fb1100;
-    padding: 10px 5px 5px 5px;
-    text-align: -webkit-center;
-}
-
-/* Center the loader */
-#preloader {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-}
-#loader {
-    display: block;
-    position: relative;
-    left: 50%;
-    top: 50%;
-    width: 150px;
-    height: 150px;
-    margin: -75px 0 0 -75px;
-    border-radius: 50%;
-    border: 3px solid transparent;
-    border-top-color: #9370DB;
-    -webkit-animation: spin 2s linear infinite;
-    animation: spin 2s linear infinite;
-}
-#loader:before {
-    content: "";
-    position: absolute;
-    top: 5px;
-    left: 5px;
-    right: 5px;
-    bottom: 5px;
-    border-radius: 50%;
-    border: 3px solid transparent;
-    border-top-color: #BA55D3;
-    -webkit-animation: spin 3s linear infinite;
-    animation: spin 3s linear infinite;
-}
-#loader:after {
-    content: "";
-    position: absolute;
-    top: 15px;
-    left: 15px;
-    right: 15px;
-    bottom: 15px;
-    border-radius: 50%;
-    border: 3px solid transparent;
-    border-top-color: #FF00FF;
-    -webkit-animation: spin 1.5s linear infinite;
-    animation: spin 1.5s linear infinite;
-}
-@-webkit-keyframes spin {
-    0%   {
-        -webkit-transform: rotate(0deg);
-        -ms-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100% {
-        -webkit-transform: rotate(360deg);
-        -ms-transform: rotate(360deg);
-        transform: rotate(360deg);
-    }
-}
-@keyframes spin {
-    0%   {
-        -webkit-transform: rotate(0deg);
-        -ms-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100% {
-        -webkit-transform: rotate(360deg);
-        -ms-transform: rotate(360deg);
-        transform: rotate(360deg);
-    }
-}
-/* Add animation to "page content" */
-.animate-bottom {
-  position: relative;
-  -webkit-animation-name: animatebottom;
-  -webkit-animation-duration: 1s;
-  animation-name: animatebottom;
-  animation-duration: 1s
-}
-
-@-webkit-keyframes animatebottom {
-  from { bottom:-100px; opacity:0 } 
-  to { bottom:0px; opacity:1 }
-}
-
-@keyframes animatebottom { 
-  from{ bottom:-100px; opacity:0 } 
-  to{ bottom:0; opacity:1 }
-}
-
-#myDiv {
-  display: none;
-  text-align: center;
-}
-#snackbar {
-    visibility: hidden;
-    min-width: 250px;
-    margin-left: -125px;
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    border-radius: 2px;
-    padding: 16px;
-    position: fixed;
-    z-index: 1;
-    left: 50%;
-    bottom: 30px;
-    font-size: 17px;
-}
-
-#snackbar.show {
-    visibility: visible;
-    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-    animation: fadein 0.5s, fadeout 0.5s 2.5s;
-}
-
-@-webkit-keyframes fadein {
-    from {bottom: 0; opacity: 0;} 
-    to {bottom: 30px; opacity: 1;}
-}
-
-@keyframes fadein {
-    from {bottom: 0; opacity: 0;}
-    to {bottom: 30px; opacity: 1;}
-}
-
-@-webkit-keyframes fadeout {
-    from {bottom: 30px; opacity: 1;} 
-    to {bottom: 0; opacity: 0;}
-}
-
-@keyframes fadeout {
-    from {bottom: 30px; opacity: 1;}
-    to {bottom: 0; opacity: 0;}
-}
 </style>
-   <script>
-var myVar;
 
-function myFunctions() {
-    myVar = setTimeout(showPage, 2000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-
-function myFunction() {
-    var x = document.getElementById("snackbar")
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
-</script>
-     
-   
+<?php if(isset($_SESSION['password'])) { ?>
+   <?php echo $_SESSION['functionget']; ?>
 </head>
+<?php echo "
+<body style='bgcolor: #1a598c;' ".$_SESSION['function'];  ?>
 
-<body style"bgcolor: #1a598c;" onload="myFunctions()"
->
 
-	<?php if(isset($_SESSION['username'])) { ?>
+	
  <div class="screen-container">        <div class="chat">
           <div class="chat-container" style"background: #1a598c;">
             <div class="user-bar">
@@ -225,26 +116,23 @@ function myFunction() {
                 <i class="zmdi zmdi-arrow-left"></i></a>
               </div>
               <div class="avatar">
-                <img src="avtar.png" alt="Avatar">
+                <img src="robotic.png" alt="Avatar">
               </div>
               <div class="name">
-                <span>Hassan web</span>
+                <span>Robotic Eye</span>
                 <span class="status">online</span>
               </div><div class="actions more">
-                <i class="zmdi zmdi-more-vert" onclick="myFunction()">
+                <i class="zmdi zmdi-more-vert">
              </i>
               </div>  
-           
+           <?php echo $_SESSION['clear']; ?>
 
 
               <div class="actions attachment"> 
 
   <i data-toggle="modal" data-target="#myModal" class="zmdi zmdi-attachment-alt" ></i>
               </div>
-              <div class="actions"> <i name='clear'  id='clear'>
-<img  src="logoutimg.png"   height=25px width=25px>
-                </i> 
-              </div>
+      
             </div>
 
 <div id="snackbar">This page is underconstruction</div>
@@ -254,27 +142,32 @@ function myFunction() {
 	
 <div class='main' >
 <?php //Check if the user is logged in or not ?>
-<?php if(isset($_SESSION['username'])) { ?>
+<?php if(isset($_SESSION['password'])) { ?>
 
-
-
-<div  class="conversation">
-
- <div id="preloader">
+<div id="preloader" >
   <div id="loader"></div>
 </div> 
   <div style="display:none;" id="myDiv" class="animate-bottom">
-<div class="conversation-container" id='result'>
-</div>
 
+<div  class="conversation">
+<div>
+   <div class="conversation-container"  id='result'>
+
+
+
+
+
+
+</div>
+</div>
 
   
 </div></div>
 
 <div class="fixed-footer">
 
-	<form method="post" onsubmit="return submitchat();" class="conversation-compose">
-                <div class="emoji" onclick="myFunction()">
+	<form method="post" onsubmit="return submitchat();" class="conversation-compose" >
+                <div class="emoji" >
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" id="smiley" x="3147" y="3209"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.153 11.603c.795 0 1.44-.88 1.44-1.962s-.645-1.96-1.44-1.96c-.795 0-1.44.88-1.44 1.96s.645 1.965 1.44 1.965zM5.95 12.965c-.027-.307-.132 5.218 6.062 5.55 6.066-.25 6.066-5.55 6.066-5.55-6.078 1.416-12.13 0-12.13 0zm11.362 1.108s-.67 1.96-5.05 1.96c-3.506 0-5.39-1.165-5.608-1.96 0 0 5.912 1.055 10.658 0zM11.804 1.01C5.61 1.01.978 6.034.978 12.23s4.826 10.76 11.02 10.76S23.02 18.424 23.02 12.23c0-6.197-5.02-11.22-11.216-11.22zM12 21.355c-5.273 0-9.38-3.886-9.38-9.16 0-5.272 3.94-9.547 9.214-9.547a9.548 9.548 0 0 1 9.548 9.548c0 5.272-4.11 9.16-9.382 9.16zm3.108-9.75c.795 0 1.44-.88 1.44-1.963s-.645-1.96-1.44-1.96c-.795 0-1.44.878-1.44 1.96s.645 1.963 1.44 1.963z" fill="#7d8489"/></svg>
                 </div>
                 <input type='text' name='chat' id='chatbox' class="input-msg" autocomplete="off" placeholder="ENTER CHAT HERE" /></input>
@@ -349,11 +242,12 @@ function myFunction() {
   </div>
 </div>
 
-
+    
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="Image-Upload-Preview-Plugin-With-jQuery-Bootstrap-img-upload/dist/js/bootstrap-imageupload.js"></script>
 
+<!--
         <script>
             var $imageupload = $('.imageupload');
             $imageupload.imageupload();
@@ -373,8 +267,28 @@ function myFunction() {
                 $(this).blur();
             });
         </script>
+-->
 
 <script>
+    function navSlide() {
+  var objDiv = $(window).scrollTop();
+
+  if (objDiv >= offset_val) { // the detection!
+   objDiv.scrollTop = objDiv.scrollHeight;
+  } else {
+    $nav_header.removeClass('is-sticky');
+  }
+}
+</script>
+
+<script>
+$("#result").scrollTop($("#result")[0].scrollHeight);
+</script>
+
+
+<script>
+
+
 // Javascript function to submit new chat entered by user
 function submitchat(){
 		if($('#chat').val()=='' || $('#chatbox').val()==' ') return false;
@@ -427,34 +341,34 @@ $(document).ready(function(){
 			success:function(data){
 				$('#result').html(data);
 			}
-		})
-	})
-})
+		});
+	});
+});
+
+
+
+
+
 </script>
 <script>
- 
 
 window.setInterval(function() {
- var elem = document.getElementById('result');
+	var elem = document.getElementById('result');
  elem.scrollTop = elem.scrollHeight;  }, 10);
-
-
-
-
-
  </script>
-
-
-
 
 <?php } else { ?>
 
 <center>
 <div class='userscreen'><center>
-<img src='whatsapp4.png' height=100px width=120px></center>
+
+<img src="hello.png" height=150px width=150px>
+</center>
 	<form method="post">
-		<input type='password' class='binput' placeholder="ENTER YOUR PASSCODE HERE" name='username' />
+	    	<input type='text' class='binput' placeholder="ENTER YOUR NAME HERE" name='username' /><BR>
+		<input type='password' class='binput' placeholder="ENTER YOUR PASSCODE HERE" name='password' />
 <center>
+    
 		<input type='submit' class='bbutton' value='START CHAT' /></center>
 
 
